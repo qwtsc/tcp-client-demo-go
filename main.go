b6be42e02f88884b2d549a1727fe5d37a4d29d4a
chong <c.shen@sjtu.edu.cn>
2020-10-14 22:00:38 +0800

    fix bug of sending email

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 )
 
 type Config struct {
-	host     string
-	port     int
-	username string
-	password string
+	Host     string `yaml:"host"`
+	Port     int `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
 }
 
 var conn *bufio.ReadWriter
@@ -27,7 +27,7 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	if err = yaml.Unmarshal(content, config); err != nil {
+	if err = yaml.Unmarshal(content, &config); err != nil {
 		panic(err)
 	}
 	tcpConn, err := net.Dial("tcp", "192.168.2.155:8813")
@@ -41,7 +41,7 @@ func init() {
 	alertMessage.SetHeader("To", "shenchongdadi@163.com")
 	alertMessage.SetHeader("Subject", "You need to change the light intensity")
 	alertMessage.SetBody("text/html", "<b>chong</b> please stand up and do you exp!")
-	mailDialer = mail.NewDialer(config.host, config.port, config.username, config.password)
+	mailDialer = mail.NewDialer(config.Host, config.Port, config.Username, config.Password)
 }
 
 func main() {
@@ -56,15 +56,19 @@ func main() {
 }
 
 func readResp(conn *bufio.ReadWriter) {
-	fmt.Println(conn.ReadString('\n'))
+	resp, err := conn.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp)
+
 }
 
 func sendCommand(command string) {
 	fmt.Fprintln(conn, command)
 	conn.Flush()
 	fmt.Println(command)
-	resp, _ := conn.ReadString('\n')
-	fmt.Println(resp)
+	readResp(conn)
 }
 
 func singleSchedule() {
