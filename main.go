473165fcd779b1c1e2d65a9a2adae848b784a32d
chong <c.shen@sjtu.edu.cn>
2020-10-14 17:28:12 +0800

    go fmt

--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
+	mail "gopkg.in/gomail.v2"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"net"
 	"time"
-	mail "gopkg.in/gomail.v2"
-	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	host string
-	port int
+	host     string
+	port     int
 	username string
 	password string
 }
 
 var conn *bufio.ReadWriter
-var mailDailer *mail.Dialer
+var mailDialer *mail.Dialer
 var alertMessage *mail.Message
 
 func init() {
@@ -41,35 +41,39 @@ func init() {
 	alertMessage.SetHeader("To", "shenchongdadi@163.com")
 	alertMessage.SetHeader("Subject", "You need to change the light intensity")
 	alertMessage.SetBody("text/html", "<b>chong</b> please stand up and do you exp!")
-	mailDailer = mail.NewDialer(config.host, config.port, config.username, config.password)
+	mailDialer = mail.NewDialer(config.host, config.port, config.username, config.password)
 }
 
 func main() {
-	for i:=0; i<10; i++ {
+	for i := 0; i < 10; i++ {
 		singleSchedule()
 		go func() {
-			if err := mailDailer.DialAndSend(alertMessage); err != nil{
+			if err := mailDialer.DialAndSend(alertMessage); err != nil {
 				fmt.Println(err)
-			}}()
+			}
+		}()
 	}
 }
 
-func readResp(conn *bufio.ReadWriter){
+func readResp(conn *bufio.ReadWriter) {
 	fmt.Println(conn.ReadString('\n'))
 }
 
-func sendCommand(command string){
+func sendCommand(command string) {
 	fmt.Fprintln(conn, command)
-	fmt.Println(conn.ReadString('\n'))
+	conn.Flush()
+	fmt.Println(command)
+	resp, _ := conn.ReadString('\n')
+	fmt.Println(resp)
 }
 
 func singleSchedule() {
 	sendCommand("record")
 	sendCommand("run 10")
-	time.Sleep(time.Second*50)
+	time.Sleep(time.Second * 50)
 	sendCommand("schedule 1800 10 2.93 1")
-	time.Sleep(time.Second*1800)
+	time.Sleep(time.Second * 1800)
 	readResp(conn) // waiting for receiving the terminal signal of schedule, otherwise, endless loop
 	sendCommand("sstop")
-	time.Sleep(time.Second*5)
+	time.Sleep(time.Second * 5)
 }
