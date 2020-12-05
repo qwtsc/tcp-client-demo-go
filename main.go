46efc20af9e597431c7d00530cec8520779639eb
qwtsc <c.shen@sjtu.edu.cn>
2020-12-05 21:20:18 +0800

    integrate motor

--- main.go
+++ main.go
@@ -28,15 +28,25 @@ type Pump struct {
 	Conn *bufio.ReadWriter
 }
 
+type Motor struct {
+	Conn *bufio.ReadWriter
+}
+
+type Valve struct {
+	Conn *bufio.ReadWriter
+}
+
 type Setup struct {
 	Pump
 	Sensor
+    Motor
+    Valve
 	MailDialer *mail.Dialer
 }
 
-func NewSetup(pump Pump, sensor Sensor, dialer *mail.Dialer) Setup{
+func NewSetup(pump Pump, sensor Sensor, motor Motor, valve Valve, dialer *mail.Dialer) Setup{
 	return Setup{
-		pump, sensor, dialer,
+		pump, sensor, motor, valve, dialer,
 	}
 }
 
@@ -47,6 +57,7 @@ func readResp(conn *bufio.ReadWriter) {
 	}
 	fmt.Print(resp)
 }
+
 
 func sendCommand(conn *bufio.ReadWriter, command string) error {
 	command = command + "\r\n"
@@ -73,7 +84,7 @@ func (pump *Pump) schedule(time int, rateInitial float32, volumn float32, slope 
 }
 
 func (pump *Pump) run(rate float32)  {
-	command := fmt.Sprintf("run %.1f", rate)
+	command := fmt.Sprintf("run %.2f", rate)
 	if err := sendCommand(pump.Conn, command); err != nil {
 		fmt.Println("run error", err)
 	}
@@ -89,6 +100,41 @@ func (pump *Pump) interrupt()  {
 		fmt.Println("interrupt error", err)
 	}
 }
+
+func (valve *Valve) openV(pos int) {
+	command := fmt.Sprintf("relay %d", pos)
+	if err := sendCommand(valve.Conn, command); err != nil {
+		fmt.Println("run error", err)
+	}
+}
+
+func (valve *Valve) closeV(pos int) {
+	command := fmt.Sprintf("rstop %d", pos)
+	if err := sendCommand(valve.Conn, command); err != nil {
+		fmt.Println("run error", err)
+	}
+}
+
+func (motor *Motor) clock(step int) {
+	command := fmt.Sprintf("motor %d", step)
+	if err := sendCommand(motor.Conn, command); err != nil {
+		fmt.Println("run error", err)
+	}
+}
+
+func (motor *Motor) antiClock(step int) {
+	command := fmt.Sprintf("antimotor %d", step)
+	if err := sendCommand(motor.Conn, command); err != nil {
+		fmt.Println("run error", err)
+	}
+}
+
+func (motor *Motor) mstop() {
+	if err := sendCommand(motor.Conn, "stopmotor"); err != nil {
+		fmt.Println("run error", err)
+	}
+}
+
 func (sensor *Sensor) record() {
 	if err := sendCommand(sensor.Conn, "record"); err != nil {
 		fmt.Println("record error", err)
@@ -101,10 +147,52 @@ func (sensor *Sensor) sstop() {
 	}
 }
 
+func (setup *Setup) testAbsorbanceRun() {
+    setup.closeAllValves()
+    for vPos := 0; vPos < 5; vPos++ {
+        setup.Valve.openV(vPos)
+        setup.record()
+        setup.Pump.run(10)
+	    time.Sleep(time.Second * 180)
+        fmt.Println("please check whether the light intensity power input is 90%")
+       // for i := 0; i < 3; i++ {
+       //     setup.Motor.antiClock(1000)
+       //     fmt.Println("1")
+       // }
+       // setup.Motor.clock(3000)
+        setup.stop()
+        setup.sstop()
+        setup.Valve.closeV(vPos)
+    }
+}
+
+func (setup *Setup) closeAllValves() {
+    for vPos := 0; vPos < 5; vPos++ {
+        setup.Valve.closeV(vPos)
+    }
+}
+
+func (setup *Setup) autoRun(secs int) {
+    setup.closeAllValves()
+    for vPos := 0; vPos < 5; vPos++ {
+        setup.Valve.openV(vPos)
+        fmt.Println("please check whether the light intensity power input is 90%")
+        setup.singleRun(secs)
+        for i := 0; i < 3; i++ {
+            setup.Motor.antiClock(1000)
+            time.Sleep(60*time.Second)
+            setup.singleRun(secs)
+        }
+        setup.Motor.clock(3000)
+        time.Sleep(60*time.Second)
+        setup.Valve.closeV(vPos)
+    }
+}
+
 func (setup *Setup) singleRun(secs int) {
 	setup.record()
 	setup.run(10)
-	time.Sleep(time.Second * 60)
+	time.Sleep(time.Second * 120)
 	setup.schedule(secs, 10, 2.93, 1)
 	time.Sleep(time.Second * time.Duration(secs))
 	readResp(setup.Pump.Conn)
@@ -124,13 +212,16 @@ func init() {
 		panic(err)
 	}
 	tcpConn, err := net.Dial("tcp", "192.168.2.155:8813")
+	tcpConnM, err := net.Dial("tcp", "192.168.2.203:8813")
 	if err != nil {
 		panic(err)
 	}
 	conn := bufio.NewReadWriter(bufio.NewReader(tcpConn), bufio.NewWriter(tcpConn))
+	connM := bufio.NewReadWriter(bufio.NewReader(tcpConnM), bufio.NewWriter(tcpConnM))
 	readResp(conn)
+    readResp(connM)
 	mailDialer := mail.NewDialer(config.Host, config.Port, config.Username, config.Password)
-	setup = NewSetup(Pump{conn}, Sensor{conn}, mailDialer)
+	setup = NewSetup(Pump{conn}, Sensor{conn}, Motor{connM}, Valve{conn}, mailDialer)
 }
 
 func soundRemainder(text string) {
@@ -142,6 +233,7 @@ func soundRemainder(text string) {
     conn.Write([]byte(text))
 }
 
+
 func main() {
 	alertMessage := mail.NewMessage()
 	alertMessage.SetHeader("From", "shenchongdadi@163.com")
@@ -149,24 +241,28 @@ func main() {
 	alertMessage.SetHeader("Subject", "You need to change the light intensity")
 	alertMessage.SetBody("text/html", "<b>chong</b> please stand up and do you exp!")
 	done := make(chan struct{}, 1)
-	go func() {
-		for i := 0; i < 5; i++ {
-			setup.singleRun(1200)
-			go func() {
-				if err := setup.MailDialer.DialAndSend(alertMessage); err != nil {
-					fmt.Println(err)
-				}
-			}()
-            go func() {
-                soundRemainder("该改变光强了")
-                time.Sleep(time.Second*10)
-                soundRemainder("该改变光强了")
-                time.Sleep(time.Second*10)
-                soundRemainder("提醒沈冲该改变光强了")
-            }()
-		}
-		done<- struct{}{}
-	}()
+    go func() {
+        setup.autoRun(1800)
+        done<- struct{}{}
+    }()
+//	go func() {
+//		for i := 0; i < 4; i++ {
+//			setup.singleRun(1800)
+//			go func() {
+//				if err := setup.MailDialer.DialAndSend(alertMessage); err != nil {
+//					fmt.Println(err)
+//				}
+//			}()
+//            go func() {
+//                soundRemainder("该改变光强了")
+//                time.Sleep(time.Second*10)
+//                soundRemainder("该改变光强了")
+//                time.Sleep(time.Second*10)
+//                soundRemainder("提醒沈冲该改变光强了")
+//            }()
+//		}
+//		done<- struct{}{}
+//	}()
 	abort := make(chan os.Signal, 1)
 	signal.Notify(abort, syscall.SIGINT)
 	select{
@@ -174,6 +270,7 @@ func main() {
 		setup.interrupt()
 		setup.stop()
 		setup.sstop()
+		setup.mstop()
 		os.Exit(1)
 	case <-done:
 		fmt.Println("Program exits without error")
